f75e611954ef1a9d857ac73d013df1599f004510
jimmygarces47 <jimmygarces4@gmail.com>
2022-03-02 01:17:31 +0000

    realice la actividad

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,86 @@
+import propTypes from "prop-types";
+
 import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div
+			className="container d-flex justify-content-center  align-items-center d-inline p-2"
+			style={{ backgroundColor: "black" }}>
+			<i
+				class="fa-thin  fa fa-clock"
+				style={{
+					color: "white",
+					backgroundColor: "#151515",
+					fontSize: "40px",
+				}}></i>
+			<div
+				className="seis d-inline p-2"
+				style={{
+					color: "white",
+					backgroundColor: "#151515",
+					fontSize: "40px",
+				}}>
+				{props.digitoCinco % 10}
+			</div>
+			<div
+				className="cinco d-inline p-2"
+				style={{
+					color: "white",
+					backgroundColor: "#151515",
+					fontSize: "40px",
+				}}>
+				{props.digitoCinco % 10}
+			</div>
+			<div
+				className="cuatro d-inline p-2"
+				style={{
+					color: "white",
+					backgroundColor: "#151515",
+					fontSize: "40px",
+				}}>
+				{props.digitoCuatro % 10}
+			</div>
+			<div
+				className="tres d-inline p-2"
+				style={{
+					color: "white",
+					backgroundColor: "#151515",
+					fontSize: "40px",
+				}}>
+				{props.digitoTres % 10}
+			</div>
+			<div
+				className="dos d-inline p-2 "
+				style={{
+					color: "white",
+					backgroundColor: "#151515",
+					fontSize: "40px",
+				}}>
+				{props.digitoDos % 10}
+			</div>
+			<div
+				className="uno d-inline p-2"
+				style={{
+					color: "white",
+					backgroundColor: "#151515",
+					fontSize: "40px",
+				}}>
+				{props.digitoUno % 10}
+			</div>
 		</div>
 	);
 };
-
+Home.proptype = {
+	digitoSeis: propTypes.number,
+	digitoCinco: propTypes.number,
+	digitoCuatro: propTypes.number,
+	digitoDos: propTypes.number,
+	digitoTres: propTypes.number,
+	digitoUno: propTypes.number,
+};
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import propTypes from "prop-types";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
@@ -9,4 +10,24 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let cont = 0;
+setInterval(function () {
+	const seis = Math.floor(cont / 100000);
+	const cinco = Math.floor(cont / 10000);
+	const cuatro = Math.floor(cont / 1000);
+	const tres = Math.floor(cont / 100);
+	const dos = Math.floor(cont / 10);
+	const uno = Math.floor(cont / 1);
+	cont++;
+	ReactDOM.render(
+		<Home
+			digitoUno={uno}
+			digitoDos={dos}
+			digitoTres={tres}
+			digitoCuatro={cuatro}
+			digitoCinco={cinco}
+			digitoSeis={seis}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
